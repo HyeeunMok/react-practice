3a453754756c2e104f0384e9ef5227674d4cdf29
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-01-24 14:24:42 -0500

    passing and extract route params

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
@@ -11,4 +12,17 @@ const post = (props) => (
     </article>
 );
 
+// const post = (props) => {
+//     console.log(props);
+//     return (
+//         <article className="Post" onClick={props.clicked}>
+//             <h1>{props.title}</h1>
+//             <div className="Info">
+//                 <div className="Author">{props.author}</div>
+//             </div>
+//         </article>
+//     );
+// }
+
 export default post;
+// export default withRouter(post);
